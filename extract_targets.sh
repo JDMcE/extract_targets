f2592092893d5d4fbd0149f502863ed6ac7bee81
John M <johnd.mcewan@gmail.com>
2024-05-01 21:48:50 +0100

    Nmap top 100 ports. Remove empty results

--- extract_targets.sh
+++ extract_targets.sh
@@ -24,8 +24,8 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-# Common ports: HTTP, HTTPS, SSH, FTP, Telnet, SMTP, SNMP, RDP, POP3, SMB, MSSQL, MySQL, VNC
-common_ports=(80 443 22 21 23 25 161 3389 110 445 1433 3306 5900)
+# Nmap top 100 ports
+common_ports=(80 23 443 21 22 25 3389 110 445 139 143 53 135 3306 8080 1723 111 995 993 5900 1025 587 8888 199 1720 465 548 113 81 6001 10000 514 5060 179 1026 2000 8443 8000 32768 554 26 1433 49152 2001 515 8008 49154 1027 5666 646 5000 5631 631 49153 8081 2049 88 79 5800 106 2121 1110 49155 6000 513 990 5357 427 49156 543 544 5101 144 7 389 8009 3128 444 9999 5009 7070 5190 3000 5432 1900 3986 13 1029 9 5051 6646 49157 1028 873 1755 2717 4899 9100 119 37)
 
 # If the -p option is used, only use the specified port
 if [ -n "$port" ]; then
@@ -48,3 +48,6 @@ for gnmap_file in "$@"; do
         fi
     done
 done
+
+# Remove all empty results
+find . -type f -empty -print -delete
